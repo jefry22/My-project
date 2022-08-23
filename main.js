8b2a01a8c2b5ea1454e743c77507a0e977275cb4
Jefferson <jeffersonpierov@gmail.com>
2022-08-22 20:32:30 -0500

    version 3

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const dessktop = document.querySelector('.desktop-menu');
-const btnEmail = document.querySelector('.email');
+const btnEmail = document.querySelector('#btnEmail');
+const desktopMenu= document.querySelector('.desktop-menu');
+const btnMenu = document.querySelector('.btn-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const btnClose = document.querySelector('.btnClose');
 
-btnEmail.addEventListener('click',btnOnClick);
+btnEmail.addEventListener('click',onClickEmail);
+btnMenu.addEventListener('click',onclickMenu);
+btnClose.addEventListener('click',onclickMenu);
 
-function btnOnClick(){
-    dessktop.classList.toggle('active');
+function onClickEmail(){
+    desktopMenu.classList.toggle('active');
+}
+function onclickMenu(){
+    mobileMenu.classList.toggle('active-mobile');
 }
